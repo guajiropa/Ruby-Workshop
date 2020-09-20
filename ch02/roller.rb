32a2aaab8eae7d49f5012331f0c55607e319a1fc
rjp-at-insight <robert.patterson@insight.com>
2020-09-19 21:43:43 -0400

    add roller.rb Activity 2.01: Dice Roller Program

---
+++ ch02/roller.rb
@@ -0,0 +1,6 @@
+#   AUTHOR      : Robert James Patterson
+#   DATE        : 09/19/2020
+#   FILE        : roller.rb
+#   SYNOPSIS    : obtain a random number printed on the screen for two scenarios: 
+#                 rolling a five-sided die and rolling two six-sided dice.
+#
