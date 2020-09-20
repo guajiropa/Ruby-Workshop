eccdeca9781887d2d7ab8f037475abe4b3106222
rjp-at-insight <robert.patterson@insight.com>
2020-09-20 16:32:54 -0400

    Started chapter 3 with truthyness.rb

---
+++ ch03/truthyness.rb
@@ -0,0 +1,26 @@
+#   AUTHOR      : Robert James Patterson
+#   DATE        : 09/19/2020
+#   FILE        : roller.rb
+#   SYNOPSIS    : The 'truthyness' of values.
+#
+
+def is_truthy(var)
+
+    if var
+        puts "The variable : #{var} is truthy!"
+    else
+        puts "The variable : #{var} is falsey!"
+    end
+
+end
+
+is_truthy(0)
+is_truthy(nil)
+is_truthy(false)
+is_truthy("")
+is_truthy(5)
+is_truthy("hello")
+is_truthy(!0)
+is_truthy(!!0)
+is_truthy(!nil)
+is_truthy(!!nil)
