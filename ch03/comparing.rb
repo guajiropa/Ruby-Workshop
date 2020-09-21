cf0dc69bc9bfbc07bcfea561ba4249f429019dff
rjp-at-insight <robert.patterson@insight.com>
2020-09-20 20:00:55 -0400

    added comparing.rb

---
+++ ch03/comparing.rb
@@ -0,0 +1,31 @@
+#   AUTHOR      : Robert James Patterson
+#   DATE        : 09/20/2020
+#   FILE        : comparing.rb
+#   SYNOPSIS    : Comparison operations in Ruby.
+#
+def compare(x, y)
+
+    if x < y
+        puts "#{x.chomp} < #{y.chomp}"
+    elsif x <= y
+        puts "#{x.chomp} <= #{y.chomp}"
+    elsif x == y
+        puts "#{x.chomp} == #{y.chomp}"
+    elsif x >= y
+        puts "#{x.chomp} >= #{y.chomp}"
+    elsif x > y 
+        puts "#{x.chomp} > #{y.chomp}"
+    end
+
+    rescue => exception
+        warn exception.message
+
+end
+
+puts "Please enter first value :"
+first_val = gets
+
+puts "Please enter second value :"
+second_val = gets
+
+compare(first_val, second_val)
